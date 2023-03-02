9a5efc8ec3288734e6228a5b94b6d33da5e96c7b
Andres Hidalgo <andres_fel.hidalgo@uao.edu.co>
2023-03-02 13:00:52 -0500

    ComponentApp Categories challenges

--- main.jsx
+++ main.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {App} from './App'
-import {FirstApp} from './FirstApp'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
+import { FirstApp } from "./FirstApp";
+import { ComponentApp } from "./ComponentApp";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <FirstApp value={0}/>
-  </React.StrictMode>,
-)
+    <FirstApp value={0} />
+    <ComponentApp/>
+  </React.StrictMode>
+);
